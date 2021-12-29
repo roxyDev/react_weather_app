dd0678e765a32c98bf80c2a808f2d6a0eab81502
Rocío Chavoya <rocio@fierita.carpathialab.com>
2021-12-28 23:52:33 -0600

    ultimos cambios

--- src/App.js
+++ src/App.js
@@ -5,23 +5,47 @@ import { WEATHER_KEY } from './keys';
 import { Component } from 'react';
 
 class App extends Component {
+  state = {
+    temperature: ``,
+    description: ``,
+    humidity: ``,
+    wind_speed: ``,
+    city: ``,
+    country: ``,
+    error: null,
+  };
   getWeather = async e => {
     e.preventDefault();
     const { city, country } = e.target.elements;
     const cityValue = city.value;
     const countryValue = country.value;
-    const API_URL =  `https://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=${WEATHER_KEY}`;
-    const response = await fetch(API_URL);
-    const data = await response.json();
-    console.log(data);
+      if (cityValue && countryValue) { 
+        const API_URL =  `https://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=${WEATHER_KEY}`;
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        console.log(this.state);
+
+        this.setState({
+          temperature: data.main.temp,
+          description: data.weather[0].description,
+          humidity: data.main.humidity,
+          wind_speed: data.wind.speed,
+          city: data.name,
+          country: data.sys.country,
+          error: null,
+        }, () => console.log(this.state));
+      } else {
+        this.setState({error: `Please enter a valid name` });
+      }
   }
+  
   render () {
     return (
       <div className="container p-4">
         <div className="row">
           <div className="col-md-4 mx-auto">
             <WeatherForm getWeather={this.getWeather}/>
-            <WeatherInfo />
+            <WeatherInfo {...this.state}/>
           </div>
         </div>
     </div>
